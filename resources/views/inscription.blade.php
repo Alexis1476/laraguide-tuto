1eb91e7f15f17ab1c0fe527c5e2e72740fe46cc7
Alexis1476 <halexisr20@hotmail.com>
2022-03-12 18:13:58 +0100

    Modifications view inscription

--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -4,9 +4,21 @@
     <form action="/inscription" method="post">
         {{--Dire que c'est bien nous qui avons effectué l'inscription--}}
         {{ csrf_field() }}
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
-        <input type="password" name="password_confirmation" placeholder="Password (Confirmation)">
+        <p><input type="email" name="email" placeholder="Email" value="{{old('email')}}"></p>
+        @if ($errors->has('email'))
+            <p>{{$errors->first('email')}}</p>
+        @endif
+
+        <p><input type="password" name="password" placeholder="Password"></p>
+        @if ($errors->has('password'))
+            <p>{{$errors->first('password')}}</p>
+        @endif
+
+        <p><input type="password" name="password_confirmation" placeholder="Password (Confirmation)"></p>
+        @if ($errors->has('password_confirmation'))
+            <p>{{$errors->first('password_confirmation')}}</p>
+        @endif
+
         <input type="submit" value="S'inscrire">
     </form>
 @endsection
