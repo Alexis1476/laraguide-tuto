43a15dc57e0b0eed5073f0b094b4527669112469
Alexis1476 <halexisr20@hotmail.com>
2022-03-12 23:57:27 +0100

    Validation des champs de connexion + check connexion

--- app/Http/Controllers/ConnexionController.php
+++ app/Http/Controllers/ConnexionController.php
@@ -15,9 +15,18 @@ class ConnexionController extends Controller
     public function traitement()
     {
         request()->validate([
-            'email' => ['required','email'],
+            'email' => ['required', 'email'],
             'password' => ['required'],
         ]);
+
+        // Authentification de laravel
+        $resultat = auth()->attempt([
+            'email' => request('email'),
+            'password' => request('password')  // Password mot clé pour laravel
+        ]);
+
+        var_dump($resultat);
+        // Laravel utilise par défaut class User.php, referencer dans auth.php
         return 'Traiter formulaire connexion';
     }
 }
--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+// Implémenter interface authentifiable
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
-class Utilisateur extends Model
+class Utilisateur extends Model implements Authenticatable
 {
+    // Implementer les méthodes de l'interface par défaut
+    use \Illuminate\Auth\Authenticatable;
+
+    // ! Redefinir les functions si besoin en fonction des attributs
+
     // Tableau avec les colonnes remplissables automatiquement
     protected $fillable = ['email', 'password'];
+
 }
